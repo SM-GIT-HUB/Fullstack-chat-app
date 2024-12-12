622b0c7ddf3143ab994dd7b63ff288e883d929ec
Soumik Majumder <soumikcode02@gmail.com>
2024-12-12 19:43:28 +0530

    deployment change

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ["https://lets-chat-fjae.onrender.com"],
+        origin: ["https://lets-chat-fjae.onrender.com", "https://tictactoe-o.netlify.app"],
         methods: ["GET", "POST"]
     }
 });
